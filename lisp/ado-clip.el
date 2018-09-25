913beb8e3dd51ef94a054b2c70c57ce9472c16b0
Bill Rising <brising@alum.mit.edu>
2018-09-25 16:58:47 -0400

    ran checkdoc on ado-clip.el
    Needed to change some variables to have lisp-like names

--- lisp/ado-clip.el
+++ lisp/ado-clip.el
@@ -34,8 +34,8 @@
 
 (defun ado-grab-something (&optional what-code)
   "If a region is selected, return the region.
-Otherwise react to what-code is (this is a mess; consider it obsolete)
-If what-code is
+Otherwise react to WHAT-CODE is (this has never been fully implemeted)
+If WHAT-CODE is
   nil, return the word at or before point,
    -2, return the entire buffer,
    -1, return the entire command containing the point
@@ -53,7 +53,7 @@ As of yet, only -2, -1, and 0 actually are implemented."
 	  (cond
 	   ((not what-code)
 		(word-at-point))
-	   ((= what-code -2) 
+	   ((= what-code -2)
 		(buffer-substring-no-properties (point-min) (point-max)))
 	   ((= what-code -1) ;; grab entire command
 		(let ((start-here
@@ -82,127 +82,135 @@ As of yet, only -2, -1, and 0 actually are implemented."
 	  )))
 
 (defun ado-command-to-clip (&optional use-dofile whole-buffer keep-whitespace)
-  "Grabs either the region, or if there is no region, the
-entire Stata command (or buffer if whole-buffer it non-nil), 
-then gets it ready to send to Stata. If use-dofile is 
-\"command\", it strips out comments and continuations, and spruces
-up semicolons if the outdated #delimit ; is in play. It also strips
-leading and trailing whitespace (including blank lines) unless 
-keep-whitespace is non-nil. The grabbing is done by \\[ado-grab-something], 
-the stripping is done by \\[ado-strip-comments], the semicolon-fixing 
-by \\[ado-convert-semicolons], and the whitespace trimming 
+  "Prepare a region or command to send to Stata.
+By default, it grabs the current command unless a region is selected.
+It then prepares the code for the Command window in Stata by stripping
+out both comments and continuations, as well as fixing semicolons
+if -#delimit ;- is on. Finally, leading and trailing whitespace (including
+blank lines) gets stripped. The resulting command(s) get passed to Stata.
+If USE-DOFILE is neither nil nor \"command\", the comments and continuations
+are left in the code, as it will be run in a do-file.
+If WHOLE-BUFFER it non-nil, the entire buffer gets grabbed.
+If KEEP-WHITESPACE is non-nil, all whitespace is left as-is.
+The grabbing is done by \\[ado-grab-something],
+the stripping is done by \\[ado-strip-comments], the semicolon-fixing
+by \\[ado-convert-semicolons], and the whitespace trimming
 by \\[ado-string-trim]."
 	(unless use-dofile
 	  (setq use-dofile "command"))
 	(let ((select-enable-clipboard t)
-		  (theString
+		  (string-to-fix
 		   (if whole-buffer
 			   (ado-grab-something -2)
 			 (ado-grab-something -1)))
 		  )
 	  (unless keep-whitespace
-		(setq theString (ado-string-trim theString)))
-	  (unless (> (length theString) 0)
+		(setq string-to-fix (ado-string-trim string-to-fix)))
+	  (unless (> (length string-to-fix) 0)
 		(if whole-buffer
 			(error "Buffer is empty")
 		  (error "No command found")))
 	  (if (string= use-dofile "command")
 		  (progn
-			(setq theString (ado-strip-comments theString))
+			(setq string-to-fix (ado-strip-comments string-to-fix))
 			(if (ado-delimit-is-semi)
-				(setq theString (ado-convert-semicolons theString)))
+				(setq string-to-fix (ado-convert-semicolons string-to-fix)))
 			)
 		(if (ado-delimit-is-semi)
-			(setq theString (concat "#delimit ;
-" theString)))
+			(setq string-to-fix (concat "#delimit ;
+" string-to-fix)))
 		) ;; testing for command
-	  (funcall interprogram-cut-function theString)
+	  (funcall interprogram-cut-function string-to-fix)
 	))
 
 (defun ado-other-to-clip (&optional where prefix suffix)
   "For putting things like 'search' and 'help' onto the clipboard.
-Made to be called from other programs only."
+Made to be called from other programs only.
+WHERE specifies what to grab (see \\[ado-grab-something]).
+PREFIX and SUFFIX are for debugging and are for the message put into
+the *Messages* buffer when the command runs."
   (let ((select-enable-clipboard t))
 	(if prefix (setq prefix (concat prefix " ")))
 	(if suffix (setq suffix (concat " " suffix)))
 	(message (concat prefix (ado-grab-something where) suffix))
-	(funcall interprogram-cut-function 										
+	(funcall interprogram-cut-function
 			 (concat prefix (ado-grab-something where) suffix))
 	))
 
 (defun ado-help-at-point-to-clip ()
-  "Puts -help <word-at-point>- on the clipboard/pasteboard. If a region is
-selected this is what is sent, instead."
+  "Puts -help <word-at-point>- on the clipboard/pasteboard.
+If a region is selected this is what is sent, instead."
   (interactive)
   (ado-other-to-clip nil "help"))
 
 (defun ado-help-command-to-clip ()
-  "Puts help for the current command (but not the prefixes) on the 
-clipboard/pasteboard. If a region is selected, this is sent, instead."
+  "Puts help for the current command on the clipboard/pasteboard.
+Ignores any prefix command. If a region is selected, this is sent, instead."
   (interactive)
   (ado-other-to-clip 0 "help"))
 
-(defun ado-strip-comments (theString)
-  "Strips out all comments from a selection line by line.
+(defun ado-strip-comments (string-to-fix)
+  "Strips out all comments from STRING-TO-FIX line by line.
 These cannot be modularized, because of ordering problems"
-  (let ((nesting 0) 
-		(returnString "") 
+  (let ((nesting 0)
+		(returnString "")
 		pareThru
 		matchString)
 	;; because the command window doesn't like tabs, turn sequences of spaces and tabs into single spaces
-	(setq theString (replace-regexp-in-string "[ 	]+" " " theString))
+	(setq string-to-fix (replace-regexp-in-string "[ 	]+" " " string-to-fix))
 	;; adding the space in case // are the last chars on a line
-	(while (setq pareThru (string-match "\\(^///\\|\\( \\|	\\)///\\|^//\\|\\( \\|	\\)//\\|/[*]\\|[*]/\\)" theString))
-	  (setq matchString (match-string 1 theString))
+	(while (setq pareThru (string-match "\\(^///\\|\\( \\|	\\)///\\|^//\\|\\( \\|	\\)//\\|/[*]\\|[*]/\\)" string-to-fix))
+	  (setq matchString (match-string 1 string-to-fix))
 	  (if (string= "*/" matchString)
 		  (error "Too many */ in a /* */-style comment"))
 	  ;; found something to investigate
 	  ;; put upto match onto the return string
-	  (setq returnString (concat returnString (substring theString 0 pareThru)))
-	  (setq theString (substring theString (+ pareThru (length matchString))))
-	  (cond 
+	  (setq returnString (concat returnString (substring string-to-fix 0 pareThru)))
+	  (setq string-to-fix (substring string-to-fix (+ pareThru (length matchString))))
+	  (cond
 	   ((or (string= matchString "///") (string= matchString " ///") (string= matchString "	///"))
 		(if (setq pareThru (string-match "
-" theString))
-			(setq theString (substring theString (1+ pareThru)))
+" string-to-fix))
+			(setq string-to-fix (substring string-to-fix (1+ pareThru)))
 		  (error "Found /// with no continuation")))
 	   ((or (string= matchString "//") (string= matchString " //") (string= matchString "	//"))
 		(if (setq pareThru (string-match "
-" theString))
-			  (setq theString (substring theString pareThru))
-		  (setq theString "")))
-	   (t 
+" string-to-fix))
+			  (setq string-to-fix (substring string-to-fix pareThru))
+		  (setq string-to-fix "")))
+	   (t
 		(setq nesting 1)
 		(while (> nesting 0)
-		  (if (not (setq pareThru (string-match "\\(/[*]\\|[*]/\\)" theString)))
+		  (if (not (setq pareThru (string-match "\\(/[*]\\|[*]/\\)" string-to-fix)))
 			  (error "Too many /* in a /* */-style comment"))
-		  (if (string= (match-string 0 theString) "*/")
+		  (if (string= (match-string 0 string-to-fix) "*/")
 			  (setq nesting (1- nesting))
 			(setq nesting (1+ nesting)))
-		  (setq theString (substring theString (+ 2 pareThru)))
+		  (setq string-to-fix (substring string-to-fix (+ 2 pareThru)))
 		  ) ;; end of nesting while loop
 		))  ;; end of cond function
 	  )  ;; end of while searching loop
-	(setq returnString (concat returnString theString))
+	(setq returnString (concat returnString string-to-fix))
 	returnString
 	))
 
-(defun ado-convert-semicolons (theString)
-  "Converts semicolons to newlines, and combines lines withoug semicolons"
-  (replace-regexp-in-string ";" "\n" 
-		(mapconcat 'identity (split-string theString "\n") " "))
+(defun ado-convert-semicolons (string-to-fix)
+  "Fixes semicolons in STRING-TO-FIX.
+Converts semicolons to newlines, and combines lines without semicolons."
+  (replace-regexp-in-string ";" "\n"
+		(mapconcat 'identity (split-string string-to-fix "\n") " "))
   )
   					   
 
-(defun ado-one-eol (theString)
-  "Looks to see if the theString ends in an eol. If it does not,
-one is appended. Nothing too complicated."
-;  (message (concat "ado-one-eol received ->" theString "<-"))
-  (unless (string-match "\n" (substring-no-properties theString -1))
-	(setq theString (concat theString "
+(defun ado-one-eol (string-to-fix)
+  "Ensures STRING-TO-FIX ends in an eol.
+If it does not, one is appended. The gets returned. Nothing too complicated."
+;  (message (concat "ado-one-eol received ->" string-to-fix "<-"))
+  (unless (string-match "\n" (substring-no-properties string-to-fix -1))
+	(setq string-to-fix (concat string-to-fix "
 ")))
-;  (message (concat "ado-one-eol wants to return ->" theString "<-"))
-  theString
+;  (message (concat "ado-one-eol wants to return ->" string-to-fix "<-"))
+  string-to-fix
   )
 
 (provide 'ado-clip)
