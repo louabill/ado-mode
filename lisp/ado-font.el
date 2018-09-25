396e0c403d7c4bf157282965dfe7bd1a9bfe32d5
Bill Rising <brising@alum.mit.edu>
2018-09-25 16:09:22 -0400

    Fixed redundant face definitions

--- lisp/ado-font.el
+++ lisp/ado-font.el
@@ -52,174 +52,132 @@
 ;;  site     -- bold and italic
 ;;  builtin  -- regular
 
-(defface ado-builtin-harmful-face
+(defface ado-builtin-harmful
   '((t :inherit font-lock-keyword-face))
   "Ado mode face used to highlight builtin commands which change
 data or the environment."
   :group 'ado-font-lock)
 
-(defvar ado-builtin-harmful-face 'ado-builtin-harmful-face)
-
-(defface ado-builtin-harmless-face
+(defface ado-builtin-harmless
   '((t :inherit font-lock-builtin-face))
   "Ado mode face used to highlight harmless builtin ado commands"
   :group 'ado-font-lock)
 
-(defvar ado-builtin-harmless-face 'ado-builtin-harmless-face)
-
-(defface ado-oldplace-harmful-face
+(defface ado-oldplace-harmful
   '((t :inherit ado-builtin-harmful-face :underline t))
   "Ado mode face used to highlight harmful commands stored in OLDPLACE."
   :group 'ado-font-lock)
 
-(defvar ado-oldplace-harmful-face 'ado-oldplace-harmful-face)
-
-(defface ado-oldplace-harmless-face
+(defface ado-oldplace-harmless
   '((t :inherit ado-builtin-harmless-face :underline t))
   "Ado mode face used to highlight harmless commands stored in OLDPLACE.
 This is the default font for OLDPLACE..."
   :group 'ado-font-lock)
 
-(defvar ado-oldplace-harmless-face 'ado-oldplace-harmless-face)
-
-(defface ado-plus-harmful-face
+(defface ado-plus-harmful
   '((t :inherit ado-builtin-harmful-face :weight bold))
   "Ado mode face used to highlight harmful commands in PLUS, i.e. 
 stuff downloaded off the net."
   :group 'ado-font-lock)
 
-(defvar ado-plus-harmful-face 'ado-plus-harmful-face)
-
-(defface ado-plus-harmless-face
+(defface ado-plus-harmless
   '((t :inherit ado-builtin-harmless-face :weight bold))
   "Ado mode face used to highlight harmless commands in PLUS, i.e. 
 stuff downloaded off the net. This is the default look."
   :group 'ado-font-lock)
 
-(defvar ado-plus-harmless-face 'ado-plus-harmless-face)
-
-(defface ado-personal-harmful-face
+(defface ado-personal-harmful
   '((t :inherit ado-builtin-harmful-face :slant italic))
   "Ado mode face used to highlight harmful PERSONAL commands."
   :group 'ado-font-lock)
 
-(defvar ado-personal-harmful-face 'ado-personal-harmful-face)
-
-(defface ado-personal-harmless-face
+(defface ado-personal-harmless
   '((t :inherit ado-builtin-harmless-face :slant italic))
   "Ado mode face used to highlight harmless PERSONAL commands. This
 is the default look"
   :group 'ado-font-lock)
 
-(defvar ado-personal-harmless-face 'ado-personal-harmless-face)
-
-(defface ado-site-harmful-face
+(defface ado-site-harmful
   '((t :inherit ado-builtin-harmful-face :slant italic :weight bold))
   "Ado mode face used to highlight harmful SITE commands."
   :group 'ado-font-lock)
-(defvar ado-site-harmful-face 'ado-site-harmful-face)
 
-(defface ado-site-harmless-face
+(defface ado-site-harmless
   '((t :inherit ado-builtin-harmless-face :slant italic :weight bold))
   "Ado mode face used to highlight harmless SITE commands. This
 is the default look"
   :group 'ado-font-lock)
-(defvar ado-site-harmless-face 'ado-site-harmless-face)
 
-(defface ado-constant-face
+(defface ado-constant
   '((t :inherit font-lock-constant-face))
   "Font Lock mode face used to highlight builtin constants."
   :group 'ado-font-lock)
-(defvar ado-constant-face 'ado-constant-face)
 
-(defface ado-platform-specific-face
+(defface ado-platform-specific
   '((t :inherit font-lock-constant-face :weight bold))
   "Ado mode face used to highlight builtin contants which exist
 only on particular platforms"
   :group 'ado-font-lock)
-(defvar ado-platform-specific-face 'ado-platform-specific-face)
 
-(defface ado-string-face
+(defface ado-string
   '((t :inherit font-lock-string-face))
   "Ado mode face used to highlight strings."
   :group 'ado-font-lock)
 
-(defvar ado-string-face 'ado-string-face)
-
-(defface ado-comment-face
+(defface ado-comment
   '((t :inherit font-lock-comment-face))
   "Ado mode face used to highlight comments."
   :group 'ado-font-lock)
 
-(defvar ado-comment-face 'ado-comment-face)
-
-(defface ado-variable-name-face
+(defface ado-variable-name
   '((t :inherit font-lock-variable-name-face))
   "Ado mode face used to highlight macro, scalar and temporary names."
   :group 'ado-font-lock)
 
-(defvar ado-variable-name-face 'ado-variable-name-face)
-
-(defface ado-matrix-name-face
+(defface ado-matrix-name
   '((t :inherit ado-variable-name-face :underline t))
   "Ado mode face used to highlight vectors and matrices"
   :group 'ado-font-lock)
 
-(defvar ado-matrix-name-face 'ado-matrix-name-face)
-
-(defface ado-function-name-face
+(defface ado-function-name
   '((t :inherit font-lock-function-name-face))
   "Ado mode face used to highlight functions in ado programs and 
 iactions in dialogs"
   :group 'ado-font-lock)
 
-(defvar ado-function-name-face 'ado-function-name-face)
-
-(defface ado-needs-subcommand-face
+(defface ado-needs-subcommand
   '((t :inherit ado-builtin-harmless-face :background "grey80"))
   "Ado-mode face used to document commands which need a subcommand
 to be correct"
   :group 'ado-font-lock)
 
-(defvar ado-needs-subcommand-face 'ado-needs-subcommand-face)
-
-(defface ado-subcommand-face
+(defface ado-subcommand
   '((t :inherit font-lock-type-face))
   "Ado mode face used to highlight subcommands or suboptions,
 and iactions which work as subcommands for dialogs."
   :group 'ado-font-lock)
 
-(defvar ado-subcommand-face 'ado-subcommand-face)
-
-(defface ado-obsolete-face
+(defface ado-obsolete
   '((t :inherit font-lock-warning-face))
   "Ado mode face used to highlight obsolete commands."
   :group 'ado-font-lock)
 
-(defvar ado-obsolete-face 'ado-obsolete-face) 
-
-(defface ado-mata-keyword-face
+(defface ado-mata-keyword
   '((t :inherit font-lock-preprocessor-face))
   "Ado mode face used to highlight mata keywords."
   :group 'ado-font-lock)
 
-(defvar ado-mata-keyword-face 'ado-mata-keyword-face)
-
-(defface ado-mata-future-keyword-face
+(defface ado-mata-future-keyword
   '((t :inherit ado-mata-keyword-face :background "linen"))
   "Ado mode face used to highlight mata keywords."
   :group 'ado-font-lock)
 
-(defvar ado-mata-future-keyword-face 'ado-mata-future-keyword-face)
-
 ;; have another mata function name face? Start by inheriting the regular function
 ;;  face and then fixing it later.
-(defface ado-mata-function-name-face
+(defface ado-mata-function-name
   '((t :inherit ado-function-name-face))
   "Ado mode face used to highlight mata functions, of all things."
   :group 'ado-font-lock)
-
-(defvar ado-mata-function-name-face 'ado-mata-function-name-face)
 
 (provide 'ado-font)
 
